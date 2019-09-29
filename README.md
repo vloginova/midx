40613c3f83741982af57a66a8c439213970aab96
Vita Loginova <vita.loginova@gmail.com>
2019-09-29 21:12:27 +0300

    Update Main and add run instructions to README

--- README.md
+++ README.md
@@ -1,4 +1,13 @@
 MIDX
 ====================
 
-MIDX is a simple library to build an index for full-text files search
+MIDX (Mini Index) is a simple library for full-text file search. It uses the trigram-based index implemented on Kotlin
+using coroutines and flows.
+
+To run the demo:
+```console
+$ git clone https://github.com/vloginova/midx.git midx
+$ cd midx
+$ ./gradlew jar
+$ java -jar build/libs/midx-1.0-SNAPSHOT.jar [--search-ignore-case] <files separated by space>
+```
--- src/main/kotlin/com/vloginova/midx/Main.kt
+++ src/main/kotlin/com/vloginova/midx/Main.kt
@@ -4,58 +4,70 @@ import com.vloginova.midx.impl.TrigramIndex
 import com.vloginova.midx.impl.buildIndexAsync
 import kotlinx.coroutines.*
 import java.io.File
+import kotlin.system.exitProcess
 import kotlin.system.measureTimeMillis
 
-const val inviteToSearch = "Please type something request to search:"
-const val inviteToWaitOrCancel = "Please wait for its completion or type 'cancel':"
-const val inviteToToBuild = "Please input a root directory path to start an index build:"
+private const val USAGE = "Usage: midx-<version>.jar [--search-ignore-case] <files separated by space>"
+private const val SEARCH_INVITATION = "search > "
+private const val CANCEL_INVITATION = "Please wait for completion or type 'cancel':"
 
-@UseExperimental(ExperimentalCoroutinesApi::class)
-fun main() {
-    println("Welcome to Midx demo.")
-    println(inviteToToBuild)
-    val root = getInputDirectory()
+fun main(args: Array<String>) {
+    if (args.isEmpty()) {
+        println(USAGE)
+    }
+    val searchIgnoreCase = args[0] == "--search-ignore-case"
+    val files = args.drop(if (searchIgnoreCase) 1 else 0)
+        .map { File(it) }
 
-    val startTime = System.currentTimeMillis()
-    val deferredIndex = buildIndexAsync(listOf(root))
+    if (files.isEmpty()) {
+        println(USAGE)
+        exitProcess(-1)
+    }
 
-    awaitInBackground(deferredIndex, startTime)
-        .invokeOnCompletion {
-            if (!deferredIndex.isCancelled) println(inviteToSearch)
-        }
+    checkFiles(files)
 
-    println("Index is building. $inviteToWaitOrCancel")
-    while (true) {
-        val line = readLine()
-        if (line.isNullOrEmpty()) continue
+    println("Indexing...")
+    println(CANCEL_INVITATION)
 
-        if (!deferredIndex.isCompleted) {
-            if (line == "cancel") {
-                cancelBuild(deferredIndex)
-                if (deferredIndex.isCancelled) return
+    runBlocking {
+        val startTime = System.currentTimeMillis()
+        val deferredIndex = buildIndexAsync(files)
+
+        awaitInBackground(deferredIndex, startTime)
+            .invokeOnCompletion {
+                if (!deferredIndex.isCancelled) print(SEARCH_INVITATION)
             }
-            println("Build is not yet completed. $inviteToWaitOrCancel")
-            continue
-        }
 
-        val index: TrigramIndex = runBlocking { deferredIndex.await() }
-        searchInIndex(index, line)
-        println(inviteToSearch)
+        while (true) {
+            if (deferredIndex.isCompleted) print(SEARCH_INVITATION)
+
+            val line = readLine()
+            if (line.isNullOrEmpty()) continue
+
+            if (!deferredIndex.isCompleted) {
+                if (line == "cancel") {
+                    cancelBuild(deferredIndex)
+                    if (deferredIndex.isCancelled) return@runBlocking
+                }
+                println(CANCEL_INVITATION)
+                continue
+            }
+
+            val index: TrigramIndex = deferredIndex.getCompleted()
+            searchInIndex(index, searchIgnoreCase, line)
+        }
     }
 }
 
-private fun getInputDirectory(): File {
-    while (true) {
-        val filePath = readLine()
-        if (filePath.isNullOrEmpty()) continue
-
-        val file = File(filePath)
-        when {
-            !file.exists() -> println("'$filePath' doesn't exist. Please try again:")
-            !file.canRead() -> println("'$filePath' cannot be read. Please try again:")
-            else -> return file
+private fun checkFiles(files: List<File>) {
+    var incorrectFileFound = false
+    files.forEach { file ->
+        if (!file.canRead()) {
+            System.err.println("'${file.path}' cannot be read.")
+            incorrectFileFound = true
         }
     }
+    if (incorrectFileFound) exitProcess(-1)
 }
 
 private fun awaitInBackground(deferredIndex: Deferred<TrigramIndex>, startTime: Long): Job =
@@ -65,31 +77,27 @@ private fun awaitInBackground(deferredIndex: Deferred<TrigramIndex>, startTime: 
         println("The index was built in ${endTime - startTime} ms.")
     }
 
-private fun cancelBuild(deferredIndex: Deferred<TrigramIndex>) {
+private suspend fun cancelBuild(deferredIndex: Deferred<TrigramIndex>) {
     val timeMillis = measureTimeMillis {
-        runBlocking {
-            deferredIndex.cancelAndJoin()
-        }
+        deferredIndex.cancelAndJoin()
     }
     if (deferredIndex.isCancelled) println("The build was cancelled in $timeMillis ms")
 }
 
-private fun searchInIndex(index: TrigramIndex, text: String) {
+private suspend fun searchInIndex(index: TrigramIndex, ignoreCase: Boolean, text: String) {
     val timeMillis = measureTimeMillis {
-        runBlocking {
-            index.searchAsync(text = text) { (file, matchingText, lineNumber, startIdx, endIdx) ->
-                prettyPrint(file.path, FontStyle.BOLD)
-                prettyPrint(" ($lineNumber line): ", FontStyle.BOLD)
+        index.searchAsync(text, ignoreCase) { (file, matchingText, lineNumber, startIdx, endIdx) ->
+            prettyPrint(file.path.takeLast(60), FontStyle.BOLD)
+            prettyPrint(" : $lineNumber:\t", FontStyle.BOLD)
 
-                print(matchingText.take(startIdx))
-                prettyPrint(
-                    matchingText.substring(startIdx, endIdx),
-                    FontStyle.BOLD,
-                    FontStyle.RED
-                )
-                println(matchingText.drop(endIdx))
-            }.await()
-        }
+            print(matchingText.take(startIdx))
+            prettyPrint(
+                matchingText.substring(startIdx, endIdx),
+                FontStyle.BOLD,
+                FontStyle.RED
+            )
+            println(matchingText.drop(endIdx))
+        }.await()
     }
     println("The search was completed in $timeMillis ms.")
 }
